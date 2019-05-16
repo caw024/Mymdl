57b5309a31e9f7b933918809573caf97c2613e6f
Calvin Aw <caw@stuy.edu>
2019-05-15 22:59:53 -0400

    cool

--- makefile
+++ makefile
@@ -1,5 +1,6 @@
-test: robot.mdl lex.py main.py matrix.py mdl.py display.py draw.py gmath.py yacc.py
+test: face.mdl myscript.mdl lex.py main.py matrix.py mdl.py display.py draw.py gmath.py yacc.py
 	python main.py face.mdl
+	python main.py myscript.mdl
 
 clean:
 	rm *pyc *out parsetab.py
--- script.py
+++ script.py
@@ -46,6 +46,78 @@ def run(filename):
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
 
-    print symbols
+    #print symbols
     for command in commands:
-        print command
+        if command['op'] == 'push':
+            stack.append( [x[:] for x in stack[-1]] )
+        elif command['op'] == 'pop':
+            stack.pop()
+        elif command['op'] == 'move':
+            #print 'MOVE\t' + str(args)
+            t = make_translate(command['args'][0],command['args'][1],command['args'][2])
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        elif command['op'] == 'rotate':
+            #print 'ROTATE\t' + str(args)
+            theta = command['args'][1] * (math.pi / 180)
+            if command['args'][0] == 'x':
+                t = make_rotX(theta)
+            elif command['args'][0]  == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        elif command['op'] == 'scale':
+            #print 'SCALE\t' + str(args)
+            t = make_scale(command['args'][0], command['args'][1], command['args'][2])
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        elif command['op'] == 'box':
+            #print 'BOX\t' + str(args)
+            add_box(tmp,
+                    command['args'][0], command['args'][1], command['args'][2],
+                    command['args'][3], command['args'][4], command['args'][5])
+            matrix_mult( stack[-1], tmp )
+            if command["constants"]:
+                draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols,command["constants"])
+            else:
+                draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+
+            tmp = []
+        elif command['op'] == 'sphere':
+            #print 'SPHERE\t' + str(args)
+            add_sphere(tmp,
+                    command['args'][0], command['args'][1], command['args'][2],
+                       command['args'][3], step_3d)
+            matrix_mult( stack[-1], tmp )
+            if command["constants"]:
+                draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, command["constants"])
+            else:
+                draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+            tmp = []
+        elif command['op'] == 'torus':
+            #print 'SPHERE\t' + str(args)
+            add_torus(tmp,
+                    command['args'][0], command['args'][1], command['args'][2],
+                      command['args'][3], command['args'][4], step_3d)
+            matrix_mult( stack[-1], tmp )
+            if command["constants"]:
+                draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols,command["constants"])
+            else:
+                draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+            tmp = []
+        elif command['op'] == 'constants':
+            pass
+        elif command['op'] == 'line':
+            add_edge( edges,
+                      command['args'][0], command['args'][1], command['args'][2],
+                      command['args'][3], command['args'][4],command['args'][5] )
+            matrix_mult( stack[-1], edges )
+            draw_lines(edges, screen, zbuffer, color)
+            edges = []
+
+        elif command['op'] == 'save':
+            save_extension(screen, command['args'][0])
+        elif command['op'] == 'display':
+            display(screen)
